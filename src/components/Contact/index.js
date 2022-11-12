52eb16595c745ad93782bb89d5c21048a05dc6ca
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-11 23:08:47 -0500

    Fixed all tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,7 +36,7 @@ function ContactForm(){
 
     return (
           <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
                 <form id="contact-form" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="name">Name:</label>
@@ -56,7 +56,7 @@ function ContactForm(){
                         </div>
                     )}
 
-                    <button type="submit">Submit</button>
+                    <button data-testid="btnTag" type="submit">Submit</button>
                 </form>
           </section>
     )
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import {render , cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+
+
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 /*
 * This will ensure that after each test, 
@@ -15,8 +24,14 @@ describe('Nav Component', () => {
     *Base Line test
     *to verify that the component is rendering
     */
-    it('Renders about component', () => {
-        render(<Nav/>)
+    it('Renders Nav component', () => {
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+          />);
     })
 
 
@@ -25,7 +40,13 @@ describe('Nav Component', () => {
     *To Compare Snapshots of the DOM node structure
     */
     it('Matches Snapshot', () =>{
-        const { asFragment } =render(<Nav/>); // returns a snapshot of the Nav component
+        const { asFragment } =  render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
+        />); // returns a snapshot of the Nav component
         expect(asFragment()).toMatchSnapshot(); // toMatchSnapshot() asserts that snapshots will match:
     })
 
@@ -34,7 +55,13 @@ describe('Nav Component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         //Arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } =render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+          />);
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     })
 })
@@ -42,7 +69,13 @@ describe('emoji is visible', () => {
 
 describe('links are visible', () => {
     it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
+      const { getByTestId } =render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+          />);
       expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
       expect(getByTestId('about')).toHaveTextContent('About me');
     });
